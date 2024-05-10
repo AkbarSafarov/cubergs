f47214ab726476b1c7527ef06056b01d89cf797c
Akbar <akbarsafarov.as@gmail.com>
2024-05-10 23:15:29 +0500

    animate

--- js/main.js
+++ js/main.js
@@ -1,7 +1,4 @@
 $(function(){
-
-	
-
     $('.anchor_link').on('click', function(e){
         e.preventDefault();
         $('html, body').animate({
@@ -9,7 +6,20 @@ $(function(){
         },500);
     });
 
-    
+    function animatePositions() {
+        var positions = ["pos1", "pos2", "pos3", "pos4", "pos5"];
+        var currentPosIndex = 0;
+
+        setInterval(function() {
+            $(".bg .elip").removeClass(positions[currentPosIndex]);
+            currentPosIndex = (currentPosIndex + 1) % positions.length;
+
+            $(".bg .elip").addClass(positions[currentPosIndex]);
+        }, 1500); 
+    }
+
+    animatePositions();
 });
+
 
 
